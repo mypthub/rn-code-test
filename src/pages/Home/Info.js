e90906d38f227b6f77d447a6fac3e1844ec4f947
Gustavo Parreira <gustavo.parreira@mypthub.net>
2020-03-16 17:07:14 +0000

    Added link to README in Info tab.

--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, Text, Linking, StyleSheet } from 'react-native';
 
 const Info = () => (
   <SafeAreaView>
-    <ScrollView />
+    <Text>Follow the instructions in the README file at:</Text>
+    <Text
+      style={styles.link}
+      onPress={() => {
+        Linking.openURL(
+          'https://github.com/mypthub/rn-code-test/blob/master/README.md',
+        );
+      }}>
+      https://github.com/mypthub/rn-code-test/blob/master/README.md
+    </Text>
   </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  link: {
+    color: 'blue',
+  },
+});
 
 export default Info;
