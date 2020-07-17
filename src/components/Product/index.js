b9187af6a0f1e93c22cd0260618ae787f09cd11c
Tristan <tristan.voiron@intempora.com>
2020-07-17 14:46:11 +0200

    Market tab with flatlist

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -2,64 +2,69 @@ import React from 'react';
 import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import UserAvatar from 'react-native-user-avatar';
 
-const Product = ({product}) => {
-  
-  var price = product.price.toFixed(2);
+const Product = ({ data }) => {
+  var price = data.price.toFixed(2);
   var price_view = null;
-  if (product.discount_type == "amount") {
-    price -= product.discount;
-    if (price == 0) {
+  if (data.discount_type === 'amount') {
+    price -= data.discount;
+    if (price === 0) {
       price_view = <Text style={styles.text}>Free</Text>;
     } else {
-      price_view = 
-      <View style={styles.price_view}>
-        <Text style={styles.text}>{price.toFixed(2)}£ - </Text>
-        <Text style={styles.text_discount}>{product.price.toFixed(2)}£</Text>
-      </View>
+      price_view = (
+        <View style={styles.price_view}>
+          <Text style={styles.text}>{price.toFixed(2)}£ - </Text>
+          <Text style={styles.text_discount}>{data.price.toFixed(2)}£</Text>
+        </View>
+      );
     }
-  } else if (product.discount_type == "percentage") {
-    price -= price * product.discount / 100;
-    if (price == 0) {
+  } else if (data.discount_type === 'percentage') {
+    price -= (price * data.discount) / 100;
+    if (price === 0) {
       price_view = <Text style={styles.text}>Free</Text>;
     } else {
-      price_view = 
-      <View style={styles.price_view}>
-        <Text style={styles.text}>{price.toFixed(2)}£ - </Text>
-        <Text style={styles.text_discount}>{product.price.toFixed(2)}£</Text>
-      </View>
+      price_view = (
+        <View style={styles.price_view}>
+          <Text style={styles.text}>{price.toFixed(2)}£ - </Text>
+          <Text style={styles.text_discount}>{data.price.toFixed(2)}£</Text>
+        </View>
+      );
     }
   } else {
-    if (price == 0) {
+    if (price === 0) {
       price_view = <Text style={styles.text}>Free</Text>;
     } else {
-      price_view = <Text style={styles.text}>{price}£</Text>
+      price_view = <Text style={styles.text}>{price}£</Text>;
     }
   }
 
   return (
-    <ImageBackground source={{uri: product.image}} style={styles.image}>
+    <ImageBackground source={{ uri: data.image }} style={styles.image}>
       <View style={styles.info_bar}>
         <View style={styles.title_view}>
           <View>
-            <Text style={styles.text}>{product.name}</Text>
+            <Text style={styles.text}>{data.name}</Text>
             {price_view}
           </View>
           <View style={styles.avatar_view}>
-          <UserAvatar size={20} src="https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" />
+            <UserAvatar
+              size={20}
+              src="https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg"
+            />
           </View>
         </View>
-        <Text style={styles.text_description}>{product.short_description}</Text>
+        <Text style={styles.text_description}>{data.short_description}</Text>
       </View>
     </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
   image: {
-    aspectRatio: 4/3,
+    aspectRatio: 4 / 3,
     justifyContent: 'flex-end',
     borderRadius: 15,
     overflow: 'hidden',
+    marginHorizontal: 10,
   },
   info_bar: {
     padding: 7,
@@ -75,11 +80,6 @@ const styles = StyleSheet.create({
   },
   avatar_view: {
     justifyContent: 'center',
-  },
-  avatar: {
-    aspectRatio: 4/3,
-    borderRadius: 15,
-    overflow: 'hidden',
   },
   text: {
     color: 'white',
--- src/pages/Home/Market.js
+++ src/pages/Home/Market.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, FlatList, View, Text } from 'react-native';
 import Product from '../../components/Product';
 import products from '../../../assets/products.json';
 
-const Market = () => (
-  <SafeAreaView>
-    <Product product={products[0]} />
-  </SafeAreaView>
-);
+const Market = () => {
+  var products_sorted = {};
+  products.forEach(product => {
+    if (product.category in products_sorted) {
+      products_sorted[product.category].splice(product.order, 0, product);
+    } else {
+      products_sorted[product.category] = [product];
+    }
+  });
+
+  const renderProducts = ({ item }) => {
+    return <Product data={item} />;
+  };
+
+  const renderLists = ({ item }) => {
+    return (
+      <View>
+        <Text style={styles.text}>{item[0].category}</Text>
+        <FlatList horizontal={true} data={item} renderItem={renderProducts} />
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={Object.values(products_sorted)}
+        renderItem={renderLists}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    fontSize: 50,
+    alignSelf: 'center',
+    marginBottom: 5,
+    marginTop: 10,
+  },
 });
 
 export default Market;
