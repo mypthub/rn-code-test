c4e127fd38956aa30f910c9bb57ba64f4124fd64
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2021-09-05 21:10:12 +0400

    CH-0016 Add Android code module

---
+++ android/app/src/main/java/com/rncodetest/scode/CodePackage.kt
@@ -0,0 +1,20 @@
+package com.rncodetest.scode
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class CodePackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+        val modules: MutableList<NativeModule> = ArrayList()
+        modules.add(CodeModule(reactContext))
+        return modules
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
+        return emptyList<ViewManager<View, ReactShadowNode<*>>>().toMutableList()
+    }
+}
---
+++ src/side-effects/service-code.ts
@@ -0,0 +1,6 @@
+import { NativeModules } from 'react-native';
+
+const { CodeModule } = NativeModules;
+
+export const getServiceCode = (): Promise<string> =>
+  CodeModule.getCode();
