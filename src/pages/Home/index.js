788b0fe19027f401d8b4eb458a71b5fb61a962a1
Bharat Kudale <bharatkudale.33@gmail.com>
2021-10-19 21:13:33 +0530

    [BK] Commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,8 +3,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Info from './Info';
 import Market from './Market';
+import FetchKey from './FetchKey';
+import ProductDetail from './ProductDetail';
+import { NavigationContainer } from '@react-navigation/native'
+import { createCompatNavigatorFactory } from '@react-navigation/compat'
+import { createStackNavigator } from '@react-navigation/stack'
+
 
 const HomeTabs = createBottomTabNavigator();
+// const MarketStack = createNativeStackNavigator();
+
+// function MarketStackScreen() {
+//   return (
+//     <MarketStack.Navigator>
+//       <MarketStack.Screen name="Market" component={Market} />
+//       <MarketStack.Screen name="ProductDetail" component={ProductDetail} />
+//     </MarketStack.Navigator>
+//   );
+// }
+const MarketStack = createCompatNavigatorFactory(createStackNavigator)({
+  Market: {
+    screen: Market,
+  },
+  ProductDetail: {
+    screen: ProductDetail,
+  } ,
+})
+
+
 
 const Home = () => {
   return (
@@ -18,7 +44,9 @@ const Home = () => {
           } else if (route.name === 'Market') {
             iconName = 'shopping-cart';
           }
-
+          else if (route.name === 'FetchKey') {
+            iconName = 'info';
+          }
           // You can return any component that you like here!
           return <MaterialIcons name={iconName} size={size} color={color} />;
         },
@@ -28,7 +56,9 @@ const Home = () => {
         inactiveTintColor: 'gray',
       }}>
       <HomeTabs.Screen name="Info" component={Info} />
-      <HomeTabs.Screen name="Market" component={Market} />
+      <HomeTabs.Screen name="Market" component={MarketStack} />
+      <HomeTabs.Screen name="FetchKey" component={FetchKey} />
+
     </HomeTabs.Navigator>
   );
 };
